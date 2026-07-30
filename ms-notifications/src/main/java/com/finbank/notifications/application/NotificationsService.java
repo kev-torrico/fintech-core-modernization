7681de1592aa926e0636e066f26907b3e7c09e3f
KevinVillca <kevtorricov@gmail.com>
2026-07-30 10:13:16 -0400

    feat: Define NotificationsService interface for managing user notifications inside the notification microservice

--- ms-notifications/src/main/java/com/finbank/notifications/application/NotificationsService.java
+++ ms-notifications/src/main/java/com/finbank/notifications/application/NotificationsService.java
@@ -1,3 +1,11 @@
-public class NotificationsService {
-    
+package com.finbank.notifications.application;
+
+import com.finbank.notifications.domain.Notifications;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public interface NotificationsService {
+    void register(UUID userId, String type, Map<String, String> payload);
+    List<Notifications> getForUser(UUID userId);
 }
