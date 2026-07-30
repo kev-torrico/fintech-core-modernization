84415ee4406a0a756cfa93099fb1ded1856db9ee
KevinVillca <kevtorricov@gmail.com>
2026-07-30 10:14:24 -0400

    feat: Define Notifications entity with fields for user ID, type, payload, and timestamps in the notification microservice

--- ms-notifications/src/main/java/com/finbank/notifications/domain/Notifications.java
+++ ms-notifications/src/main/java/com/finbank/notifications/domain/Notifications.java
@@ -1,3 +1,41 @@
+package com.finbank.notifications.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import java.time.Instant;
+import java.util.Map;
+import java.util.UUID;
+
+@Entity
+@Table(schema = "notifications", name = "notifications")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Notifications {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NotificationsType type;
+
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "jsonb", nullable = false)
+    private Map<String, String> payload;
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "read_at")
+    private Instant readAt;
 }
