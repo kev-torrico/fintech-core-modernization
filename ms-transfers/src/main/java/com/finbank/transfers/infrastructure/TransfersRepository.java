7c0d9b230c0b53eba03a03abd5295167927bb7dd
KevinVillca <kevtorricov@gmail.com>
2026-07-31 15:19:19 -0400

    feat: Add TransfersRepository for managing transfer data in the transfer microservice

---
+++ ms-transfers/src/main/java/com/finbank/transfers/infrastructure/TransfersRepository.java
@@ -0,0 +1,11 @@
+package com.finbank.transfers.infrastructure;
+
+import com.finbank.transfers.domain.Transfers;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+import java.util.UUID;
+
+public interface TransfersRepository extends JpaRepository<Transfers, UUID> {
+    List<Transfers> findBySourceAccountIdOrTargetAccountIdOrderByCreatedAtDesc(
+        UUID sourceAccountId, UUID targetAccountId);
+}
