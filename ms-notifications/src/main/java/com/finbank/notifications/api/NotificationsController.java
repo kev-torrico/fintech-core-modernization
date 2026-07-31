d243903bbdc7d5c0d326d23d7e7c27370ed669ad
KevinVillca <kevtorricov@gmail.com>
2026-07-31 15:35:51 -0400

    feat: Update request mappings in NotificationsController and TransfersController to include API versioning

--- ms-notifications/src/main/java/com/finbank/notifications/api/NotificationsController.java
+++ ms-notifications/src/main/java/com/finbank/notifications/api/NotificationsController.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/notifications")
+@RequestMapping("/api/v1/notifications")
 @RequiredArgsConstructor
 public class NotificationsController {
 
--- ms-transfers/src/main/java/com/finbank/transfers/api/TransfersController.java
+++ ms-transfers/src/main/java/com/finbank/transfers/api/TransfersController.java
@@ -16,7 +16,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/transfers")
+@RequestMapping("/api/v1/transfers")
 @RequiredArgsConstructor
 public class TransfersController {
 
